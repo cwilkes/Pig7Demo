4aff5f243a8ea77a11b660ce1dd3ab84309b34b9
Chris Wilkes <cwilkes@gmail.com>
2010-10-21 15:58:45 -0700

    01ungroup split into name,movie,rating rows

--- cascading/src/main/java/org/seattlehaoop/demo/cascading/distance/MovieDistances.java
+++ cascading/src/main/java/org/seattlehaoop/demo/cascading/distance/MovieDistances.java
@@ -2,6 +2,7 @@ package org.seattlehaoop.demo.cascading.distance;
 
 import static org.seattlehaoop.demo.cascading.distance.FlowUtils.makeLocalFlow;
 import cascading.flow.Flow;
+import cascading.operation.function.UnGroup;
 import cascading.operation.regex.RegexSplitter;
 import cascading.pipe.Each;
 import cascading.pipe.Pipe;
@@ -9,11 +10,18 @@ import cascading.tuple.Fields;
 
 public class MovieDistances {
 
+	private static final String MOVIE = "movie";
+	
 	public static Pipe makePipe() {
 		Pipe pipe = new Pipe("movieReviews");
 
 		// line is tab delimited
 		pipe = new Each(pipe, new Fields("line"), new RegexSplitter("\t"));
+
+		// takes "JimBob Mo1 1.2 Mo2 2.3 Mo3 9.9" and splits into 3 different
+		// rows, one for each (movie,rate) pair
+		// also puts field names on the resulting fields
+		pipe = new Each(pipe, new UnGroup(new Fields("name", MOVIE, "rate"), new Fields(0), 2));
 
 		return pipe;
 	}
